11f9cfc4b7862c6f4a8e459695ec8b96a3fd2264
Brendan Whitfield <brendanw@windworksdesign.com>
2014-05-01 19:03:17 -0400

    added basic game loop

--- Planetary.py
+++ Planetary.py
@@ -2,12 +2,17 @@
 
 
 import pygame
-from gi.repository import Gtk
+from pygame.locals import QUIT
+
+#from gi.repository import Gtk
 
 
 class Planetary:
+
     def __init__(self):
-        pass
+    	self.running = True
+        self.window =pygame.display.get_surface()
+        self.clock = pygame.time.Clock()
 
     # Called to save the state of the game to the Journal.
     def write_file(self, file_path):
@@ -19,14 +24,34 @@ class Planetary:
 
     # The main game loop.
     def run(self):
-        pass
+
+    	# The main game loop.
+    	while self.running:
+
+    		#read the daily news
+    		for event in pygame.event.get():
+    			if event.type == QUIT:
+    				pygame.quit()
+    				self.running = False
+
+    		#list for the update regions
+    		update_regions = list()
+
+    		#game logic
+
+    		#update the window surface
+
+    		#keep at 30fps
+    		self.clock.tick(30)
+
+
 
 
 # This function is called when the game is run directly from the command line:
 # ./Planetary.py
 def main():
     pygame.init()
-    pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+    pygame.display.set_mode((1200, 900), pygame.RESIZABLE)
     game = Planetary()
     game.run()
 
